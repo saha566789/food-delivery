aeb5e57d3a7e3109eefd59f32e55cf7936158d1f
saha566789 <tomasaha3456@gmail.com>
2024-06-15 23:58:28 +0600

    banner added home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,11 @@ import "./Header.css"
 const Header = () => {
     return (
         <div className="header">
-            
+            <div className="header-contents">
+                <h2>Order your favorite food here</h2>
+                <p>Relaxed atmosphere with a diverse menu offering a range of dishes, typically served at moderate prices.Quick-service restaurants offering standardized menu items with efficient service.</p>
+                <button>ViewMenu</button>
+            </div>
         </div>
     );
 };
